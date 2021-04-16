d469058bf0696f3c50976e0e12d75a425ccc8c7e
Qrow99 <bkeyro99@gmail.com>
2021-04-15 22:19:47 -0700

    Multiple updates
    New movement, new tiles

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,37 +4,75 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movement_speed = 5f;
+    public float movement_speed = .5f;
+    public Transform movepoint;
     private bool hasitem;
     public Rigidbody2D rb;
     public Animator animator;
     public bool canMove;
+    public LayerMask Walls;
+    public LayerMask Pushable;
     Vector2 movement; //stores an x and a y
+    void Start()
+    {
+        movepoint.parent = null;
 
+    }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     { //will take inputs, do not put physics here because framerate can vary
 
         if (!canMove)
         {
-            movement.x = 0;
-            movement.y = 0;
+            animator.SetFloat("Speed", 0);
             animator.SetFloat("Horizontal", 0);
             animator.SetFloat("Vertical", 0);
-            animator.SetFloat("Speed", 0);
             return;
         }
-        this.enabled = true;
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
 
+        transform.position = Vector3.MoveTowards(transform.position, movepoint.position, movement_speed * Time.fixedDeltaTime);
+        if (Vector3.Distance(transform.position, movepoint.position) <= 0.5f)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if (!(Physics2D.OverlapCircle(movepoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, Walls)))
+                {
+                    if ((Physics2D.OverlapCircle(movepoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, Pushable)))
+                    {
+                        if (!(Physics2D.OverlapCircle(movepoint.position + (2 * (new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f))), 0.2f, Walls)) && !(Physics2D.OverlapCircle(movepoint.position + (2 * (new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f))), 0.2f, Pushable)))
+                        {
+                            movepoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                        }
+                    }
+                    else
+                    {
+                        movepoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    }
+                }
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!(Physics2D.OverlapCircle(movepoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, Walls)))
+                {
+                    if ((Physics2D.OverlapCircle(movepoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, Pushable)))
+                    {
+                        if (!(Physics2D.OverlapCircle(movepoint.position + (2 * (new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f))), 0.2f, Walls)) && !(Physics2D.OverlapCircle(movepoint.position + (2 * (new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f))), 0.2f, Pushable)))
+                        {
+                            movepoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                        }
+                    }
+                    else
+                    {
+
+                        movepoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    }
+                }
+            }
+        }
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-    }
-
-    void FixedUpdate()
-    { //does movement, called at a constant 50 frames per second
-        rb.MovePosition(rb.position + movement * movement_speed * Time.fixedDeltaTime); //Time.fixedDeltaTime keeps time that has elapsed since last function call, helps keep movement speed constant
     }
 }
